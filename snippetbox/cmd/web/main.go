78db67e0a6cc49a9a18e7f22446cf42b4f815bfe
Александр <akonovalov.dev@gmail.com>
2023-06-30 19:01:54 +0300

    adds new connection to database

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
 	"os"
@@ -24,10 +26,25 @@ type application struct {
 */
 func main() {
 	addr := flag.String("addr", ":4000", "Сетевой адрес веб-сервера")
+	// Определение нового флага из командной строки для настройки MySQL подключения.
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "Название MySQL источника данных")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// Чтобы функция main() была более компактной, мы поместили код для создания
+	// пула соединений в отдельную функцию openDB(). Мы передаем в нее полученный
+	// источник данных (DSN) из флага командной строки.
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	// Мы также откладываем вызов db.Close(), чтобы пул соединений был закрыт
+	// до выхода из функции main().
+	// Подробнее про defer: https://golangs.org/errors#defer
+	defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
@@ -41,8 +58,24 @@ func main() {
 	}
 
 	infoLog.Printf("Запуск сервера на %s", *addr)
-	err := srv.ListenAndServe()
+	// Поскольку переменная `err` уже объявлена в приведенном выше коде, нужно
+	// использовать оператор присваивания =
+	// вместо оператора := (объявить и присвоить)
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+// Функция openDB() обертывает sql.Open() и возвращает пул соединений sql.DB
+// для заданной строки подключения (DSN).
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
 
 // Второй MAIN (до переноса маршрутов)
